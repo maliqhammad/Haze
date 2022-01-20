683e9b4e378fa242fe4dcea752afac2a48ba1b81
hammadnaveed91@gmail.com <Malik@0335>
2022-01-20 21:39:22 +0500

    7 commit

--- app/src/main/java/com/hammad/iphoneringtones/classes/NetworkConnectivity.java
+++ app/src/main/java/com/hammad/iphoneringtones/classes/NetworkConnectivity.java
@@ -7,11 +7,26 @@ import android.net.NetworkInfo;
 public abstract class NetworkConnectivity {
 
     public static boolean isInternetConnected(Context context) {
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (!networkInfo.isConnected()) {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI")) {
+                if (ni.isConnected()) {
+                    haveConnectedWifi = true;
+                }
+            }
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE")) {
+                if (ni.isConnected()) {
+                    haveConnectedMobile = true;
+                }
+            }
+        }
+        if (!haveConnectedWifi && !haveConnectedMobile) {
             ShowSingletonAlertDialog.newInstance(context).showDialog();
         }
-        return networkInfo.isConnected();
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }
